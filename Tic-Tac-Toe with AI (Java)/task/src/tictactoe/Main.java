22c4bb747e3d178c2e3be1380772a4404f4dae08
zzuob <af19846@bristol.ac.uk>
2023-04-20 15:56:43 +0100

    Add initial state validation loop

--- Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Main.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Grid grid = null;
-        try (Scanner scan = new Scanner(System.in)) {
-            System.out.print("Enter the cells: ");
-            String symbols = scan.nextLine();
-            grid = new Grid(symbols);
-        } catch (Exception e) {
-            System.out.printf("Error: %s", e.getMessage());
+        Grid grid;
+        while(true) {
+            try {
+                Scanner scan = new Scanner(System.in);
+                System.out.print("Enter the cells: ");
+                if (scan.hasNextLine()) {
+                    String symbols = scan.nextLine();
+                    grid = new Grid(symbols);
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.printf("Error: %s\n", e.getMessage());
+            }
         }
-        assert grid != null;
         grid.printGrid();
     }
 }
