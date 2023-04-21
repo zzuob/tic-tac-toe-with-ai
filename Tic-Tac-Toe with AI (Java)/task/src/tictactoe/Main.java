f4bcb230792e64694ad17a85a54aaca2bb3e2add
zzuob <af19846@bristol.ac.uk>
2023-04-21 15:23:24 +0100

    Implement game loop

--- Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Main.java
@@ -6,10 +6,18 @@ public class Main {
         // create a blank board
         Grid grid = new Grid("_________");
         State state = new State(grid);
-        grid.printGrid();
-        Turn.takeUserTurn(grid, Symbol.X);
-        grid.printGrid();
         state.updateStatus(grid);
-        System.out.println(state.getStatus().getMessage());
+        while("UNFINISHED".equals(state.getStatus().name())) {
+            grid.printGrid();
+            Symbol player = grid.checkNextMove();
+            if ("X".equals(player.name())) {
+                Turn.takeUserTurn(grid, Symbol.X);
+            } else {
+                Turn.takeUserTurn(grid, Symbol.O);
+            }
+            state.updateStatus(grid);
+            System.out.println(state.getStatus().getMessage());
+        }
+        grid.printGrid();
     }
 }
